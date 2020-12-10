9efbddb6c6468a9b3f7e714973cb922ca4822210
NPeters216 <46542173+NPeters216@users.noreply.github.com>
2020-12-10 17:58:40 -0500

    Add files via upload

---
+++ pa4/pa4/src/cen4010/pa4/Reminder.java
@@ -0,0 +1,24 @@
+package cen4010.pa4;
+import java.util.*;  
+import java.util.ArrayList;
+import java.util.Scanner;
+import java.util.Timer;
+import java.util.TimerTask;
+
+
+
+class Reminder{
+	Timer timer;
+	public boolean timeRem = true;
+	
+	public Reminder() {
+		timer = new Timer();
+		timer.schedule(new RemindTask(), 600 * 1000);
+	}
+	class RemindTask extends TimerTask{
+		public void run() {
+			timeRem = false;
+		}
+	}
+	
+}
---
+++ pa4/pa4/src/cen4010/pa4/Wait.java
@@ -0,0 +1,13 @@
+package cen4010.pa4;
+import java.util.*;  
+import java.util.ArrayList;
+import java.util.Scanner;
+import java.lang.Thread;
+
+
+class Wait{
+	public boolean ready = true;
+	
+	
+	
+}
